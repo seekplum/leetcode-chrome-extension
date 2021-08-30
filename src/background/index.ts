77c663ad86b0e57b9cd819f2433ccf4316309a3c
huangjiandong <huangjiandong@meideng.net>
2021-08-30 22:35:20 +0800

    ADD: background js增加监听

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,27 @@
 console.log('This is background page!');
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+        (details) => {
+            if (!details.requestHeaders) return {};
+            const requestHeaders = [];
+            for (let n = 0; n < details.requestHeaders.length; n += 1) {
+                const name = details.requestHeaders[n].name.toLowerCase();
+                if (name === 'x-referrer') {
+                    requestHeaders.push({
+                        name: 'Referer',
+                        value: details.requestHeaders[n].value,
+                    });
+                } else if (name !== 'referer') {
+                    requestHeaders.push({ name, value: details.requestHeaders[n].value });
+                }
+            }
+            return { requestHeaders };
+        },
+        {
+            urls: ['*://leetcode-cn.com/*'],
+        },
+        ['requestHeaders', 'blocking', 'extraHeaders'],
+    );
+});
 
 export default undefined;
--- src/contents/all/index.tsx
+++ src/contents/all/index.tsx
@@ -1,3 +1,16 @@
 import './style.scss';
 
-console.log(`Current page's url must be prefixed with https://github.co`);
+// 注册 window 变量到 dom 中
+// const tempScript = document.createElement('script');
+// tempScript.setAttribute('type', 'text/javascript');
+// // 获取变量有延时，通过 setTimeout 处理
+// tempScript.innerHTML =
+//     "setTimeout(function() {document.body.setAttribute('data-vds', window.vds && JSON.stringify(window.vds));}, 1000)";
+// document.head?.appendChild(tempScript);
+// document.head?.removeChild(tempScript);
+
+// chrome.runtime.onMessage.addListener((request, _sender, sendResp) => {
+//     if (request.from === 'popup' && request.action === 'vds') {
+//         sendResp(document.body.getAttribute('data-vds'));
+//     }
+// });
